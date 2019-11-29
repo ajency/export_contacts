c4570d2649f258f1584368ceac0140d3ec5b185c
ralph <ralph@ajency.in>
2019-11-29 17:25:39 +0530

    sample test

--- lambda_function.py
+++ lambda_function.py
@@ -10,50 +10,75 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 
 
-nextEmailCredIndex = 0
-nextLinkedInCredIndex = 0
 email_credentials = [
 	{"username": "pnitin3103@gmail.com", "password": "ajency#123"},
-	{"username": "stinfordpaulie18@yahoo.com", "password": "ajency#123"},		#microsoft outlook & yahoo
-	{"username": "stinfordpaulie18@aol.com", "password": "ajency#123"},			#aol
-	{"username": "alina.jose1102@gmail.com", "password": "ajency#123"},			#gmail
+	# {"username": "stinfordpaulie18@yahoo.com", "password": "ajency#123"},		#microsoft outlook & yahoo
+	# {"username": "stinfordpaulie18@aol.com", "password": "ajency#123"},			#aol
+	# {"username": "alina.jose1102@gmail.com", "password": "ajency#123"},			#gmail
 ]
 
 linkedin_credentials = [
-	# {"username": "pnitin3103@gmail.com", "password": "ajency#123"},
+	{"username": "pnitin3103@gmail.com", "password": "ajency#123"},
 	# {"username": "alina.jose1102@gmail.com", "password": "ajency#123"},
-	{"username": "ralph110293@gmail.com", "password": "ajency#123"},
+	# {"username": "ralph110293@gmail.com", "password": "ajency#123"},
 ]
 
 def lambda_handler(event, context):
-	global email_credentials
-	global linkedin_credentials
-	global nextLinkedInCredIndex
-
 	hostname = socket.gethostname()
 	IPAddr = socket.gethostbyname(hostname)
 	print("Your Computer Name is:" + hostname)    
 	print("Your Computer IP Address is:" + IPAddr) 
+
 	# Program Execution
+	driver = initialize_driver()
 	print("Running in Headless browser mode..")
-	select_different_linked_in_account(nextLinkedInCredIndex)
 
-	# print("Reading message from Queue..")
-	# print(receiveMessageFromSQSQueue("https://sqs.us-east-1.amazonaws.com/452266237699/process-queue", "us-east-1"))
+	switch_to_gmail_account(driver)
+	switch_to_linkedin_account(driver)
 
+	logout_from_linkedin(driver)
+	logout_from_gmail(driver)
+	# # import_contacts
+	# import_contacts(driver, gmailUsername)
 	print("Execution Completed")
+	# End Driver
+	driver.close()
 	sys.exit()
 
 
 
+# def select_different_linked_in_account(nextLinkedInCredIndex):
+# 	driver = initialize_driver()
+# 	# driver = webdriver.Chrome(executable_path=driver_path)
+# 	hostname = socket.gethostname()
+# 	IPAddr = socket.gethostbyname(hostname)
+# 	print("Your Computer Name is:" + hostname)    
+# 	print("Your Computer IP Address is:" + IPAddr) 
+# 	# Program Execution
+# 	print("Running in Headless browser mode..")
+
+# 	switch_to_linkedin_account(driver)
+# 	switch_to_gmail_account(driver)
+
+# 	logout_from_linkedin(driver)
+# 	logout_from_gmail(driver)
+
+
 # Initialize Driver & login into LinkedIn
-def select_different_linked_in_account(nextLinkedInCredIndex):
+def initialize_driver():
+	global driver
 	# operating system 
 	driver_path = "assets/chrome_driver/chromedriver"
 	headless_chromium_path = "/assets/chrome_driver/headless-chromium"
 
+	# PROXY = "127.0.1.1:8000"
+	# PROXY = "88.157.149.250:8080"
+
 	# Headless Browser
 	chrome_options = webdriver.ChromeOptions()
+
+	# chrome_options.add_argument('--proxy-server=%s' % PROXY)
+
 	chrome_options.add_argument('--headless')
 	chrome_options.add_argument('--disable-gpu')
 	chrome_options.add_argument('--disable-dev-shm-usage')
@@ -64,22 +89,11 @@ def select_different_linked_in_account(nextLinkedInCredIndex):
 	chrome_options.add_argument('--single-process')
 	chrome_options.binary_location = os.getcwd() + headless_chromium_path
 
-	# chrome_options.add_argument('--window-size=1280x1696')
-	# chrome_options.add_argument('--user-data-dir=/tmp/user-data')
-	# chrome_options.add_argument('--hide-scrollbars')
-	# chrome_options.add_argument('--enable-logging')
-	# chrome_options.add_argument('--log-level=0')
-	# chrome_options.add_argument('--v=99')
-	# chrome_options.add_argument('--data-path=/tmp/data-path')
-	# chrome_options.add_argument('--ignore-certificate-errors')
-	# chrome_options.add_argument('--homedir=/tmp')
-	# chrome_options.add_argument('--disk-cache-dir=/tmp/cache-dir')
-
+	# return webdriver.Chrome(executable_path=driver_path)
 	driver = webdriver.Chrome(executable_path=driver_path, chrome_options=chrome_options)
-	# driver = webdriver.Chrome(executable_path=driver_path)
+	return driver
 
-	switch_to_linkedin_account(driver, nextLinkedInCredIndex)
-	logout_from_linkedin(driver)
+
 
 
 
@@ -91,6 +105,7 @@ def switch_to_linkedin_account(driver, nextCredIndex=0):
 	global nextLinkedInCredIndex
 	linkedinUsername = linkedin_credentials[nextCredIndex]['username']
 	linkedinPassword = linkedin_credentials[nextCredIndex]['password']
+
 	login_to_linkedin(driver, linkedinUsername, linkedinPassword)
 	nextLinkedInCredIndex = nextCredIndex + 1
 	isLinkedInLoggedIn = False
@@ -98,7 +113,7 @@ def switch_to_linkedin_account(driver, nextCredIndex=0):
 	try:
 		# check if login was successful
 		driver.get("https://www.linkedin.com/mynetwork/import-contacts/")
-		time.sleep(2)
+		time.sleep(1)
 		confirmLogIn = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '//*[@id="nav-settings__dropdown-trigger"]/div/li-icon')))
 		print("LinkedIn login for "+linkedinUsername+" was successful")
 		isLinkedInLoggedIn = True
@@ -108,40 +123,13 @@ def switch_to_linkedin_account(driver, nextCredIndex=0):
 		print(driver.current_url)
 		print("LinkedIn login for "+linkedinUsername+" failed")
 		isLinkedInLoggedIn = False
-		payload = {
-			"driver": driver,
-			"exception": e,
-			"redirected_url": driver.current_url,
-			"request_data": {
-				"linkedinUsername": linkedinUsername,
-				"linkedinPassword": linkedinPassword,
-			},
-		}
 		if nextLinkedInCredIndex < len(linkedin_credentials):
-			select_different_linked_in_account(nextLinkedInCredIndex)
+			switch_to_linkedin_account(driver, nextLinkedInCredIndex)
+			# select_different_linked_in_account(nextLinkedInCredIndex)
 		else:
 			print("No more linkedIn accounts available")
-
-	try:
-		while isLinkedInLoggedIn and nextEmailCredIndex < len(email_credentials):
-			print(email_credentials[nextEmailCredIndex]['username'])
-			# switch_to_diff_account(driver, nextEmailCredIndex)
-			nextEmailCredIndex += 1
-	except Exception as e:
-		print(driver.current_url)
-		print(e)
-		payload = {
-			"driver": driver,
-			"exception": e,
-			"redirected_url": driver.current_url,
-			"request_data": {
-				"emailUsername": email_credentials[nextEmailCredIndex]['username'],
-				"emailPassword": email_credentials[nextEmailCredIndex]['password'],
-				"linkedinUsername": linkedinUsername,
-				"linkedinPassword": linkedinPassword,
-			},
-		}
 		pass
+
 
 
 # LogIn function for LinkedIn Account
@@ -158,11 +146,25 @@ def login_to_linkedin(driver, username, password):
 		# submit_form
 		login = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '//*[@id="app__container"]/main/div/form/div[3]/button')))
 		login.click()
-		print(driver.current_url)
-		logged_in = WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.ID, 'nav-settings__dropdown-trigger')))
-		print("LOGIN SUCCESS - no error")
+		try:
+			verify_email = WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.ID, 'input__email_verification_pin')))
+			print("Email verification required")
+			verify_email.clear()
+			user_input = raw_input("Please enter the email verification code sent to the email ID")
+			verify_email.send_keys(user_input)
+			confirm = WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.ID, 'email-pin-submit-button')))
+			driver.execute_script("arguments[0].click();", confirm)
+			pass
+		except Exception as e:
+			try:
+				WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.ID, 'recaptcha-anchor')))
+				print("Recaptcha verification required")
+				pass
+			except Exception as e:
+				print("Logged In")
+				pass
+			pass
 	except Exception as e:
-		print("LOGIN EXCEPTION")
 		print(e)
 		print("Current URL : "+driver.current_url)
 		payload = {
@@ -178,16 +180,6 @@ def login_to_linkedin(driver, username, password):
 		pass
 
 
-
-# def debugger(driver):
-# 	print(driver.current_url)
-# 	driver.save_screenshot("screenshot_temp.png")
-# 	s3 = boto3.client('s3')
-# 	s3.create_bucket(Bucket='testbuckethp3pytest')
-# 	# s3.upload_file('screenshot_temp.png', 'testbuckethp3pytest', 'screenshot_temp.png')
-# 	s3.Bucket('exportlinkedincontacts').put_object(Key='screenshot.png', Body=driver.save_screenshot("screenshot_temp.png"))
-
-
 # LogOut function for LinkedIn Account
 def logout_from_linkedin(driver):
 	print("Logging out from linkedIn")
@@ -200,17 +192,94 @@ def logout_from_linkedin(driver):
 		logout = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '//*[@id="nav-settings__dropdown-options"]/li[5]/ul/li/a')))
 		logout.click()
 	except Exception as e:
-		payload = {
-			"driver": driver,
-			"exception": e,
-			"redirected_url": driver.current_url,
-			"request_data": [],
-		}
 		print("LOGOUT EXCEPTION")
 		print(e)
 		print(driver.current_url)
-	# End Driver
-	driver.close()
+
+
+
+def switch_to_gmail_account(driver, nextCredIndex=0):
+	global email_credentials
+	gmailUsername = email_credentials[nextCredIndex]['username']
+	gmailPassword = email_credentials[nextCredIndex]['password']
+	login_to_gmail(driver, gmailUsername, gmailPassword)
+	nextCredIndex += 1
+	time.sleep(3)
+	try:
+		# check if login was successful
+		confirmLogIn = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '//*[@id="gb"]/div[2]/div[3]/div/div[2]/div/a')))
+		print("Gmail Log In for "+gmailUsername+" was successful")
+	except Exception as e:
+		print(e)
+		print(driver.current_url)
+		print("Gmail login for "+gmailUsername+" failed")
+		if nextCredIndex < len(email_credentials):
+			switch_to_gmail_account(driver, nextCredIndex)
+		else:
+			print("No more gmail accounts available")
+		
+
+
+# LogIn function for Gmail Account
+def login_to_gmail(driver, username, password):
+	print("Logging In into Gmail as "+username)
+	time.sleep(1)
+	try:
+		driver.get("https://accounts.google.com/")
+		driver.find_element_by_id('identifierId').send_keys(username)
+		driver.find_element_by_id("identifierNext").click()
+		time.sleep(5)
+		pwd = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '//*[@id="password"]/div[1]/div/div[1]/input')))
+		pwd.send_keys(password)
+		# form submit
+		# pwd.send_keys(Keys.RETURN)
+		login = driver.find_element_by_id("passwordNext")
+		login.click()
+		try:
+			WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.ID, 'recaptcha-anchor')))
+			print("Recaptcha verification required")
+			# WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.ID, 'input__email_verification_pin')))
+			# print("Email verification required")
+			pass
+		except Exception as e:
+			pass
+	except Exception as e:
+		print("Page elements were not found")
+		
+
+
+# recaptcha-anchor
+# recaptcha-token
+# recaptcha-verify-button
+
+
+
+# LogOut function for Gmail Account
+def logout_from_gmail(driver):
+	print("Logging out from Gmail")
+	try:
+		driver.get("https://accounts.google.com/")
+		# Logout drop down
+		clk = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '//*[@id="gb"]/div[2]/div[3]/div/div[2]/div/a')))
+		clk.click()
+		# Logout
+		logout = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "gb_71")))
+		logout.click()
+		# try:
+		# 	WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '//*[@id="view_container"]')))
+		# 	removeAccounttClk = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '//*[@id="view_container"]/div/div/div[2]/div/div/div/form/span/section/div/div/div/div/ul/li[3]')))
+		# 	removeAccounttClk.click()
+		# 	selectClk = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '//*[@id="view_container"]/div/div/div[2]/div/div/div/form/span/section/div/div/div/div[1]/ul/li[1]')))
+		# 	selectClk.click()
+		# 	removeClk = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '//*[@id="yDmH0d"]/div[4]/div/div[2]/div[3]/div[1]/span')))
+		# 	removeClk.click()
+		# 	time.sleep(3)
+		# 	print("Remove the account from browser")
+		# except Exception as e:
+		# 	pass
+	except Exception as e:
+		print(e)
+
 
 
 
@@ -291,5 +360,7 @@ def deleteMessageFromSQSQueue(queue_url, region_name, receipt_handle):
 
 
 
-
-lambda_handler(None, None)
+#span attr('email') security-noreply@linkedin.com
+# class="h7 bg ie"
+# //*[@id=":d3"]/div[1]/table/tbody/tr/td/center/table/tbody/tr[2]/td/table/tbody/tr/td/table/tbody/tr[3]/td/p/strong
+# lambda_handler(None, None)
