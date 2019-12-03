88e6c38ed7aba8a1094fd414a6ea98f0ca313106
ralph <ralph@ajency.in>
2019-12-03 17:33:00 +0530

    class defined code

--- config.py
+++ config.py
@@ -1,19 +1,20 @@
-return {
+import os,sys,time,csv,datetime,platform
+# config
+nextLinkedInCredIndex = 0
+
+email_credentials = [
+	{"username": "pnitin3103@gmail.com", "password": "ajency#123"},
+	{"username": "stinfordpaulie18@yahoo.com", "password": "ajency#123"},		#microsoft outlook & yahoo
+	{"username": "stinfordpaulie18@aol.com", "password": "ajency#123"},			#aol
+	{"username": "alina.jose1102@gmail.com", "password": "ajency#123"},			#gmail
+]
+
+linkedin_credentials = [
+	{"username": "pnitin3103@gmail.com", "password": "ajency#123"},
+	{"username": "alina.jose1102@gmail.com", "password": "ajency#123"},
+]
+
+config = {
 	"aws_region": "us-east-1",
-
-	"driver_path": "assets/chrome_driver/chromedriver",
-	"headless_chromium_path": "/assets/chrome_driver/headless-chromium",
-
-	"email_credentials": [
-		{"username": "pnitin3103@gmail.com", "password": "ajency#123"},
-		{"username": "stinfordpaulie18@yahoo.com", "password": "ajency#123"},		#microsoft outlook & yahoo
-		{"username": "stinfordpaulie18@aol.com", "password": "ajency#123"},			#aol
-		{"username": "alina.jose1102@gmail.com", "password": "ajency#123"},			#gmail
-	],
-
-	"linkedin_credentials": [
-		{"username": "pnitin3103@gmail.com", "password": "ajency#123"},
-		{"username": "alina.jose1102@gmail.com", "password": "ajency#123"},
-	],
-
 }
+
