86f36a627089931e7a083f1b645307370149eeb3
ralph <ralph@ajency.in>
2019-12-03 17:24:36 +0530

    include code for all account types

--- lambda_function.py
+++ lambda_function.py
@@ -1,3 +1,4 @@
+import re
 import json
 import boto3
 import pickle
@@ -16,15 +17,15 @@ from selenium.webdriver.common.by import By
 logging.basicConfig(filename = 'app.log', level = logging.INFO)
 
 email_credentials = [
-	{"username": "pnitin3103@gmail.com", "password": "ajency#123"},
-	# {"username": "stinfordpaulie18@yahoo.com", "password": "ajency#123"},		#microsoft outlook & yahoo
+	# {"username": "pnitin3103@gmail.com", "password": "ajency#123"},
+	{"username": "stinfordpaulie18@yahoo.com", "password": "ajency#123"},		#microsoft outlook & yahoo
 	# {"username": "stinfordpaulie18@aol.com", "password": "ajency#123"},			#aol
-	{"username": "alina.jose1102@gmail.com", "password": "ajency#123"},			#gmail
+	# {"username": "alina.jose1102@gmail.com", "password": "ajency#123"},			#gmail
 ]
 
 linkedin_credentials = [
 	{"username": "pnitin3103@gmail.com", "password": "ajency#123"},
-	{"username": "alina.jose1102@gmail.com", "password": "ajency#123"},
+	# {"username": "alina.jose1102@gmail.com", "password": "ajency#123"},
 	# {"username": "ralph110293@gmail.com", "password": "ajency#123"},
 ]
 nextEmailCredIndex = 0
@@ -68,6 +69,7 @@ def lambda_handler(event, context):
 	# # delete_all_cookies(driver)
 
 def go_to_step(driver):
+	global nextEmailCredIndex
 	global nextLinkedInCredIndex
 	print("")
 	print("Choose your next step as below:")
@@ -80,7 +82,8 @@ def go_to_step(driver):
 	user_input = input("Enter your step: ")
 	method_exec = user_input.strip().upper()
 	if method_exec == "A":
-		switch_to_gmail_account(driver)
+		# switch_to_gmail_account(driver)
+		switch_to_diff_account(driver, nextEmailCredIndex)
 		go_to_step(driver)
 	elif method_exec == "B":
 		switch_to_linkedin_account(driver, nextLinkedInCredIndex)
@@ -100,6 +103,11 @@ def go_to_step(driver):
 		driver.close()
 		sys.exit()
 		# go_to_step(driver)
+
+
+
+
+
 
 
 
@@ -210,6 +218,11 @@ def initialize_new_driver():
 
 
 
+
+
+
+
+
 def check_login_to_linkedin(driver):
 	is_loggedin = False
 	try:
@@ -227,6 +240,7 @@ def check_login_to_linkedin(driver):
 		return is_loggedin
 		pass
 	return is_loggedin
+
 
 def check_login_to_gmail(driver):
 	is_loggedin = False
@@ -377,6 +391,246 @@ def switch_to_gmail_account(driver, nextCredIndex=0):
 				logging.info("No more gmail accounts available")
 		
 
+
+
+
+def switch_to_diff_account(driver, nextCredIndex=0):
+	global email_credentials
+	global nextEmailCredIndex
+	username = email_credentials[nextCredIndex]['username']
+	password = email_credentials[nextCredIndex]['password']
+	if re.search("yahoo", username):
+		try:
+			login_to_yahoo(driver, username, password)
+			# check if login was successful
+			driver.get("https://login.yahoo.com/account/")
+			clk = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '//*[@id="ybarAccountMenu"]')))
+			driver.execute_script("arguments[0].click();", clk)
+			time.sleep(1)
+			WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '//*[@id="ybarAccountMenu"]')))
+			print("Yahoo Log In for "+username+" was successful")
+		except Exception as e:
+			print(e)
+			print("Yahoo Login for "+username+" failed")
+			logging.debug("Yahoo Login for "+username+" failed")
+		try:
+			# import_contacts
+			# import_contacts(driver, username, "yahoo")
+			# contactDataList = export_contacts(driver)
+			# export_contacts_to_db(contactDataList)
+			logout_from_yahoo(driver)
+			# remove_synced_accounts(driver)
+		except Exception as e:
+			print(e)
+			pass
+	elif re.search("aol", username):
+		try:
+			login_to_aol(driver, username, password)
+			# check if login was successful
+			driver.get("https://login.aol.com/account/")
+			# driver.find_element_by_xpath('//*[@id="navigation-menu-user-account"]/div/a[2]')
+			print("AOL Log In for "+username+" was successful")
+		except Exception as e:
+			print(e)
+			print("AOL Login for "+username+" failed")
+			logging.debug("AOL Login for "+username+" failed")
+		try:
+			# import_contacts
+			# import_contacts(driver, username, "aol")
+			# contactDataList = export_contacts(driver)
+			# export_contacts_to_db(contactDataList)
+			logout_from_aol(driver)
+			# remove_synced_accounts(driver)
+		except Exception as e:
+			print(e)
+			pass
+	else:
+		try:
+			login_to_gmail(driver, username, password)
+			# check if login was successful
+			confirmLogIn = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '//*[@id="gb"]/div[2]/div[3]/div/div[2]/div/a')))
+			print("Gmail Log In for "+username+" was successful")
+			logging.debug("Gmail Log In for "+username+" was successful")
+		except Exception as e:
+			print(e)
+			print("Gmail Login for "+username+" failed")
+			logging.debug("Gmail Login for "+username+" failed")
+		try:
+			# import_contacts
+			# import_contacts(driver, username, "gmail")
+			# contactDataList = export_contacts(driver)
+			# export_contacts_to_db(contactDataList)
+			logout_from_gmail(driver)
+			# remove_synced_accounts(driver)
+		except Exception as e:
+			print(e)
+			pass
+	check_for_account_in_outlook(driver, username, password)
+
+# Check if email ID has outlook account
+def check_for_account_in_outlook(driver, username, password):
+	try:
+		login_to_outlook(driver, username, password)
+		# check if login was successful
+		driver.get("https://account.microsoft.com/")
+		WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, 'mectrl_body_signOut')))
+		print("OutLook Log In for "+username+" was successful")
+		# import_contacts
+		import_contacts(driver, username, "outlook")
+		contactDataList = export_contacts(driver)
+		export_contacts_to_db(contactDataList)
+		logout_from_outlook(driver)
+		remove_synced_accounts(driver)
+	except Exception as e:
+		# print(e)
+		print("Microsoft Outlook account may not be present for "+username)
+
+
+# LogIn function for Yahoo Account
+def login_to_yahoo(driver, username, password):
+	print("Logging In into Yahoo as "+username)
+	try:
+		driver.get("https://login.yahoo.com/account")
+		user = driver.find_element_by_id('login-username')
+		user.clear()
+		user.send_keys(username)
+		# driver.find_element_by_id("persistent").send_keys('y')
+		driver.find_element_by_id("login-signin").click()
+		time.sleep(5)
+		pwd = driver.find_element_by_id("login-passwd")
+		pwd.send_keys(password)
+		# form submit
+		# pwd.send_keys(Keys.RETURN)
+		login = driver.find_element_by_id("login-signin")
+		login.click()
+		time.sleep(2)
+	except Exception as e:
+		print(e)
+		print("Page elements were not found")
+		logging.debug("Page elements were not found")
+	# driver.save_screenshot("2.png")
+
+
+# LogOut function for Yahoo Account
+def logout_from_yahoo(driver):
+	print("Logging out from Yahoo")
+	try:
+		driver.get("https://login.yahoo.com/account/")
+		# Logout drop down
+		# clk = driver.find_element_by_id("ybarAccountMenu")
+		clk = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '//*[@id="ybarAccountMenu"]')))
+		driver.execute_script("arguments[0].click();", clk)
+		# Logout
+		logout = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '//*[@id="ybarAccountMenuBody"]/a[3]')))
+		logout.click()
+	except Exception as e:
+		print(e)
+
+
+# LogIn function for OutLook Account
+def login_to_outlook(driver, username, password):
+	print("Logging In into OutLook as "+username)
+	try:
+		driver.get("https://login.live.com/")
+		time.sleep(2)
+		user = driver.find_element_by_id('i0116')
+		user.clear()
+		user.send_keys(username)
+		driver.find_element_by_id("idSIButton9").click()
+		time.sleep(2)
+		try:
+			txt = driver.find_element_by_id("usernameError").text
+			print(txt)
+		except Exception as e:
+			pwd = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '//*[@id="i0118"]')))
+			pwd.send_keys(password)
+			# form submit
+			# pwd.send_keys(Keys.RETURN)
+			login = driver.find_element_by_id("idSIButton9")
+			login.click()
+		time.sleep(2)
+	except Exception as e:
+		print(e)
+		print("Page elements were not found")
+		logging.debug("Page elements were not found")
+	# driver.save_screenshot("2.png")
+
+
+# LogOut function for Gmail Account
+def logout_from_outlook(driver):
+	try:
+		driver.get("https://account.microsoft.com/")
+		time.sleep(2)
+		# Logout drop down
+		# clk = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, 'mectrl_headerPicture')))
+		clk = driver.find_element_by_id("mectrl_headerPicture")
+		clk.click()
+		# Logout
+		logout = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, 'mectrl_body_signOut')))
+		print("loggging out from Microsoft OutLook")
+		# logout = driver.find_element_by_id("mectrl_body_signOut")
+		logout.click()
+		time.sleep(2)
+	except Exception as e:
+		pass
+
+
+# LogIn function for AOL Account
+def login_to_aol(driver, username, password):
+	print("Logging In into AOL as "+username)
+	try:
+		driver.get("https://login.aol.com/")
+		time.sleep(2)
+		user = driver.find_element_by_id('login-username')
+		user.clear()
+		user.send_keys(username)
+		driver.find_element_by_id("login-signin").click()
+		time.sleep(1)
+		try:
+			# Not a Robot
+			clk = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '//*[@id="recaptcha-anchor"]/div[1]')))
+			driver.execute_script("arguments[0].click();", clk)
+			# driver.find_element_by_xpath('//*[@id="recaptcha-anchor"]/div[1]').click()
+			time.sleep(5)
+			driver.find_element_by_xpath('//*[@id="recaptcha-submit"]').click()
+			# clk2 = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "recaptcha-submit")))
+			# driver.execute_script("arguments[0].click();", clk2)
+		except Exception as e:
+			print("Validated: Not a Robot")
+			pass
+		time.sleep(3)
+		pwd = driver.find_element_by_id('login-passwd')
+		pwd.send_keys(password)
+		# form submit
+		# pwd.send_keys(Keys.RETURN)
+		login = driver.find_element_by_id("login-signin")
+		login.click()
+		time.sleep(2)
+	except Exception as e:
+		print(e)
+		print("Page elements were not found")
+		logging.debug("Page elements were not found")
+	# driver.save_screenshot("2.png")
+
+
+# LogOut function for Gmail Account
+def logout_from_aol(driver):
+	print("Logging out from AOL")
+	try:
+		driver.get("https://login.aol.com/account/")
+		# # Logout drop down
+		# # clk = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, 'ybarAccountMenu')))
+		# clk = driver.find_element_by_id("ybarAccountMenu")
+		# clk.click()
+		# Logout
+		# logout = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '//*[@id="ybarAccountMenuBody"]/a')))
+		logout = driver.find_element_by_xpath('//*[@id="navigation-menu-user-account"]/div/a[2]')
+		logout.click()
+		time.sleep(2)
+	except Exception as e:
+		print(e)
+
+
 # LogIn function for Gmail Account
 def login_to_gmail(driver, username, password):
 	print("Logging In into Gmail as "+username)
@@ -414,19 +668,20 @@ def logout_from_gmail(driver):
 	else:
 		print("Logging out from Gmail")
 		logging.info("Logging out from Gmail")
-		try:
-			driver.get("https://accounts.google.com/")
-			# Logout drop down
-			clk = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '//*[@id="gb"]/div[2]/div[3]/div/div[2]/div/a')))
-			clk.click()
-			# Logout
-			logout = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "gb_71")))
-			logout.click()
-			print('Gmail logout success')
-			logging.info('Gmail logout success')
-		except Exception as e:
-			print(e)
-			logging.error('Current URL: '+driver.current_url+'\n Gmail logout failed - ' + str(e))
+		# try:
+		# 	# driver.get("https://accounts.google.com/")
+		# 	# # Logout drop down
+		# 	# clk = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '//*[@id="gb"]/div[2]/div[3]/div/div[2]/div/a')))
+		# 	# clk.click()
+		# 	# # Logout
+		# 	# logout = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "gb_71")))
+		# 	# logout.click()
+		# 	print('Gmail logout success')
+		# 	logging.info('Gmail logout success')
+		# except Exception as e:
+		# 	print(e)
+		# 	logging.error('Current URL: '+driver.current_url+'\n Gmail logout failed - ' + str(e))
+		driver.get("https://www.google.com/accounts/Logout")
 	# remove previously loggedin accounts
 	remove_previous_loggedin_gmail_accounts(driver)
 		
@@ -450,6 +705,132 @@ def remove_previous_loggedin_gmail_accounts(driver):
 
 
 
+
+
+# Import contacts from Gmail to LinkedIn
+def import_contacts(driver, username, account_tag):
+	print("Importing contacts from account("+username+")")
+	global linkedin_credentials
+	driver.get("https://www.linkedin.com/mynetwork/import-contacts/")	
+	time.sleep(3)
+
+	try:
+		if account_tag.strip().lower() == 'aol':
+			# AOL = #oauth2-agree
+			clk = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '//*[@id="ember63"]/a')))
+			clk.click()
+			time.sleep(3)
+			if len(driver.window_handles) > 1:
+				# switch the pop-up window
+				driver.switch_to.window(driver.window_handles[1])
+				time.sleep(5)
+				# Check if any account needs to be selected
+				confirmAccount = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, 'oauth2-agree')))
+				driver.execute_script("arguments[0].click();", confirmAccount)
+		elif account_tag.strip().lower() == 'outlook':
+			# OutLook = #idBtn_Accept
+			clk = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '//*[@id="ember58"]/a')))
+			clk.click()
+			time.sleep(3)
+			if len(driver.window_handles) > 1:
+				# switch the pop-up window
+				driver.switch_to.window(driver.window_handles[1])
+				time.sleep(5)
+				# Check if any account needs to be selected
+				confirmAccount = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, 'idBtn_Accept')))
+				driver.execute_script("arguments[0].click();", confirmAccount)
+		elif account_tag.strip().lower() == 'yahoo':
+			# yahoo = #oauth2-agree
+			clk = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '//*[@id="ember53"]/a')))
+			clk.click()
+			time.sleep(3)
+			if len(driver.window_handles) > 1:
+				# switch the pop-up window
+				driver.switch_to.window(driver.window_handles[1])
+				time.sleep(5)
+				# Check if any account needs to be selected
+				confirmAccount = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, 'oauth2-agree')))
+				driver.execute_script("arguments[0].click();", confirmAccount)
+		else:
+			clk = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '//*[@id="ember48"]/a')))
+			clk.click()
+			time.sleep(3)
+			if len(driver.window_handles) > 1:
+				# switch the pop-up window
+				driver.switch_to.window(driver.window_handles[1])
+				time.sleep(5)
+				# Check if any account needs to be selected
+				accountSelector = '//*[@id="view_container"]/div/div/div[2]/div/div/div/form/span/section/div/div/div/div/ul/li[1]/div/div[1]/div/div[2]/div[1]'
+				account = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, accountSelector)))
+				account.click()
+				confirmAccount = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, 'submit_approve_access')))
+				# confirmAccount.click()
+				driver.execute_script("arguments[0].click();", confirmAccount)
+		
+		#switch back to original window
+		time.sleep(0.5)
+		if len(driver.window_handles) > 1:
+			try:
+				backToPrevWindow = WebDriverWait(driver.window_handles[1], 10).until(EC.presence_of_element_located((By.XPATH, '//*[@id="minimal-util-nav"]/ul/li[1]/a')))
+				backToPrevWindow.click()
+			except Exception as e:
+				if len(driver.window_handles) > 1:
+					driver.close()
+				print("return to LinkedIn")
+	except Exception as e:
+		print(e)
+
+	driver.switch_to.window(driver.window_handles[0])
+
+	try:
+		time.sleep(3)
+		WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '//*[@id="contact-select-checkbox"]')))
+		print("Contacts imported from account["+username+"] to LinkedIn")
+	except Exception as e:
+		print(e)
+		print("Error in importing contacts from account["+username+"]")
+		continueExec = get_user_input("continue execution with same LinkedIn Account (Default:No)? (y/n) ", 'n')
+		if continueExec.strip().lower() == 'n' and nextLinkedInCredIndex < len(linkedin_credentials):
+			logout_from_linkedin(driver)
+			select_different_linked_in_account(nextLinkedInCredIndex)
+		else:
+			print("continuing with same linkedIn account for remaining accounts sync..")
+
+
+# Remove previous synced gmail accounts
+def remove_synced_accounts(driver):
+	driver.get("https://www.linkedin.com/mynetwork/settings/manage-syncing/")
+	time.sleep(5)
+	try:
+		removeAllClk = driver.find_element_by_xpath('//*[@id="ember44"]/div[1]/button')
+		removeAllClk.click()
+		rmvclk2Selector = '//*[@id="artdeco-modal-outlet"]/div/div/div[2]/div/ul/li[2]/button'
+		rmvClk2 = driver.find_element_by_xpath(rmvclk2Selector)
+		rmvClk2.click()
+	except Exception as e:
+		print(e)
+		try:
+			listResults = driver.find_elements_by_xpath('//*[@id="ember42"]/section/ul/div') # //ul[@class="list-style-none.mh5"]/div
+			for account in listResults or []:
+				rmvClk = account.find_element_by_xpath('.//li/div/button')
+				driver.execute_script("arguments[0].click();", rmvClk)
+				rmvclk2Selector = '//*[@id="artdeco-modal-outlet"]/div/div/div[2]/div/ul/li[2]/button[@class="js-mn-manage-source-confirm"]'
+				rmvClk2 = driver.find_element_by_xpath(rmvclk2Selector)
+				driver.execute_script("arguments[0].click();", rmvClk2)
+		except Exception as e:
+			# print(e)
+			# print("Removal of synced failed")
+			pass
+		pass
+	time.sleep(1)
+	driver.get("https://www.linkedin.com/mynetwork/import-contacts/saved-contacts/")
+	time.sleep(5)
+	try:
+		WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '//*[@id="ember42"]/div/div/div[1]/div/section/div[1]/p')))
+		print("Unable to remove synced accounts")
+	except Exception as e:
+		print("Removal of synced accounts was successful")
+	time.sleep(1)
 
 
 
