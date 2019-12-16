503a71b382b6c18f2506875770cf914647067290
ralph <ralph@ajency.in>
2019-12-16 11:13:23 +0530

    proxy fix

--- proxy_list.py
+++ proxy_list.py
@@ -2,8 +2,20 @@ from selenium import webdriver
 from pathlib import Path
 
 co = webdriver.ChromeOptions()
+user_agent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36'
+
 co.add_argument("log-level=3")
-co.add_argument("--headless")
+co.add_argument('--headless')
+co.add_argument('--disable-gpu')
+co.add_argument('--disable-dev-shm-usage')
+# co.add_argument('--window-size=1920,1080')
+# co.add_argument('--single-process')
+co.add_argument('--no-sandbox') # required when running as root user. otherwise you would get no sandbox errors.
+co.add_argument('--user-agent={'+user_agent+'}')
+co.add_argument('--allow-running-insecure-content')
+co.add_argument('--disable-web-security')
+co.add_argument('--no-referrers')
+co.add_argument("'chrome.prefs': {'profile.managed_default_content_settings.images': 2}")
 
 def get_proxies(co=co):
     import platform
